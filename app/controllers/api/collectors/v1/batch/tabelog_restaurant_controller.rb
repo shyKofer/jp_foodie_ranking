2b72c78984bd645a650d05c278c0aa2e385373f6
kofer <shyfoodiegrammer@gmail.com>
2019-11-02 03:05:49 +0900

    Tabelog Batch

--- app/controllers/api/collectors/v1/batch/tabelog_restaurant_controller.rb
+++ app/controllers/api/collectors/v1/batch/tabelog_restaurant_controller.rb
@@ -1,42 +1,41 @@
 class Api::Collectors::V1::Batch::TabelogRestaurantController < ApplicationController
 
-    # POST:  http://localhost:3000/api/collectors/v1/batch/tabelog_restaurant
+    # api/collectors/v1/batch/tabelog_restaurant
+    # Method : POST
     def create()
         logger.debug("Start")
-        create_restaurants()
-        #@crawling_area_uris = TabelogUri.all
-        #render json: @crawling_area_uris
+        crawl_tabelog_restaurants()
         logger.debug("End")
     end
 
-    def create_restaurants()
-        logger.debug("create_restaurants Starts")
-        @area_uris = TabelogUri.all
-        for uri_info in @area_uris
-            crawling_uri = uri_info.area_uri
+    def crawl_tabelog_restaurants()
+        @tabelog_uris = TabelogUri.all
+        for tabelog_uri in @tabelog_uris
+            crawling_uri = tabelog_uri.area_uri
             logger.debug("crawling_uri:#{crawling_uri}")
-            restaurant_cnt = crawl_area_restaurant_cnt(crawling_uri);
-            logger.debug("restaurant_cnt:#{restaurant_cnt}")
-            crawling_page_cnt = get_page_cnt(restaurant_cnt)
+            crawling_page_cnt = get_page_cnt(crawling_uri)
             logger.debug("crawling_page_cnt:#{crawling_page_cnt}")
             save_restaurants(crawling_uri, crawling_page_cnt)
         end
-        logger.debug("create_restaurants End")
     end
 
+    # エリアのレストランの件数を取得
     def crawl_area_restaurant_cnt(uri)
         dom = Nokogiri::HTML(open(uri))
-        # crawl total count of restaurants
         return Integer(dom.css(".c-page-count__num")[2].text)
     end
 
-    def get_page_cnt(restaurant_cnt)
+    # ページ数を取得
+    def get_page_cnt(crawling_uri)
+        restaurant_cnt = crawl_area_restaurant_cnt(crawling_uri)
+        logger.debug("restaurant_cnt:#{restaurant_cnt}")
         max_posting_cnt_per_page = 20
         page_cnt = restaurant_cnt / max_posting_cnt_per_page
         page_cnt += (restaurant_cnt % max_posting_cnt_per_page) > 0 ? 1 : 0
-        return page_cnt > 60 ? 60 : page_cnt
+        return page_cnt >= 60 ? 60 : page_cnt
     end
 
+    # 1ページ当りレストラン数を取得
     def get_posting_cnt_per_page(posting_no_range_dom)
         posting_cnt_from = Integer(posting_no_range_dom[0].text)
         posting_cnt_to   = Integer(posting_no_range_dom[1].text)
@@ -45,31 +44,27 @@ class Api::Collectors::V1::Batch::TabelogRestaurantController < ApplicationContr
         return (posting_cnt_to - posting_cnt_from) + 1
     end
 
+    # レストラン情報をクローリングしてDBに保存
     def save_restaurants(uri, crawling_page_cnt)
-        logger.debug("uri:#{uri}")
-        page = 1
-        crawling_page_cnt.times do
-            sleep 0.5
+        for page in (1..crawling_page_cnt) do
+            sleep 0.3
             crawling_page_url = uri + "rstLst/" + page.to_s
             logger.debug("crawling_page_url:#{crawling_page_url}")
             restaurants_html = Nokogiri::HTML(open(crawling_page_url))
             posting_cnt = get_posting_cnt_per_page(restaurants_html.css('.c-page-count__num strong'))
             logger.debug("posting_cnt:#{posting_cnt}")
-            posting_index = 0
-            posting_cnt.times do
+
+            for posting_index in (0..posting_cnt-1) do
                 begin
-                    logger.debug("posting_cnt:#{posting_cnt}")
                     logger.debug("crawling_page_url:#{crawling_page_url}")
-                    logger.debug("page:#{page}")
                     logger.debug("posting_index:#{posting_index}")
                     @restaurant = TabelogRestaurantInfo.new
                     restaurant_info_dom = restaurants_html.css('.js-open-new-window')[posting_index]
-                    @restaurant.uri_id  = Integer(restaurants_html.css('.cpy-rst-name')[posting_index][ 'href' ].split("/")[6].strip)
+                    @restaurant.uri_id  = Integer(restaurants_html.css('.cpy-rst-name')[posting_index]['href'].split("/")[6].strip)
                     logger.debug("uri_id:#{@restaurant.uri_id}")
                     @restaurant.name = restaurant_info_dom.css('.cpy-rst-name').text
                     logger.debug("name:#{@restaurant.name}")
                     if @restaurant.name.nil? || @restaurant.name.blank?
-                        posting_index += 1
                         next
                     end
                     @restaurant.genre = restaurant_info_dom.css('.cpy-area-genre').text.split("/")[1].strip
@@ -88,13 +83,11 @@ class Api::Collectors::V1::Batch::TabelogRestaurantController < ApplicationContr
                     logger.debug("dinner_budget:#{@restaurant.dinner_budget}")
                     logger.debug("@restaurant:#{@restaurant.inspect}")
                     @restaurant.save
-                    posting_index += 1
                 rescue Exception
-                    posting_index += 1
                     next
                 end
+
             end
-            page += 1
         end
     end
 
